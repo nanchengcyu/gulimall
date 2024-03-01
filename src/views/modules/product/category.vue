b11a42d80cc158735f9c43671f02fd4d2358821a
nanchengcyu <1146137024@qq.com>
2024-03-02 01:24:24 +0800

    前端初始化 以及三级目录

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<script setup>
+export default {
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get',
+      }).then(data=>{
+        console.log("成功获取到菜单数据",data)
+      })
+    }
+  },
+  created() {
+    this.getMenus();
+  }
+};
+</script>
+
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<style scoped lang="scss">
+
+</style>
