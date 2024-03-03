a1c434d5dbea57bdbeda3ca115dd8699a7dee828
nanchengcyu <1146137024@qq.com>
2024-03-03 21:22:31 +0800

    三级菜单拖拽等细节

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -56,7 +56,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -68,7 +68,15 @@ public class CategoryController {
 
         return R.ok();
     }
+    /**
+     * 批量修改
+     */
+    @RequestMapping("/update/sort")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category)); //数组转为集合
 
+        return R.ok();
+    }
     /**
      * 修改
      */
