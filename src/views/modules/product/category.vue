6a19d812837c521d6a219d952b3e8487275c3976
nanchengcyu <1146137024@qq.com>
2024-03-03 21:24:17 +0800

    三级菜单拖拽等细节

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,12 +1,16 @@
 <script setup xmlns="http://www.w3.org/1999/html">
 
 
-import {data} from "autoprefixer";
+import draggable from "element-ui/packages/color-picker/src/draggable";
 
 export default {
   data() {
     return {
-      category: {name: "", parentCid: 0, cateLevel: 0, showStatus: 1, sort: 0},
+      draggable: false,
+      maxLevel: 0,
+      title: "",
+      dialogType: "",
+      category: {name: "", parentCid: 0, cateLevel: 0, showStatus: 1, sort: 0, catId: null, productUnit: "", icon: ""},
       dialogVisible: false,
       expendedKey: [],
       menus: [],
@@ -17,6 +21,91 @@ export default {
     };
   },
   methods: {
+    batchSave() {
+
+    },
+    allowDrop(draggingNode, dropNode, type) {
+      var level = countNodeLevel(draggingNode)
+      return false;
+    },
+    countNodeLevel(draggingNode) {
+    },
+
+    submitData() {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      }
+      if (this.dialogType == "edit") {
+        this.editCategory();
+      }
+
+    },
+    batchDelete() {
+      let catIds = [];
+      let checkedNodes = this.$refs.menuTree.getCheckedNodes();
+      console.log("被选中的元素", checkedNodes);
+      for (let i = 0; i < checkedNodes.length; i++) {
+        catIds.push(checkedNodes[i].catId);
+      }
+      this.$confirm(`是否批量删除【${catIds}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(catIds, false)
+          }).then(({ data }) => {
+            this.$message({
+              message: "菜单批量删除成功",
+              type: "success"
+            });
+            this.getMenus();
+          });
+        })
+        .catch(() => {});
+    },
+    editCategory() {
+      var {catId, name, icon, productUnit} = this.category;
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update'),
+        method: 'post',
+        data: this.$http.adornData({catId, name, icon, productUnit}, false)
+      }).then(({data}) => {
+        this.$message({
+          message: '保存菜单成功',
+          type: 'success'
+        });
+        //关闭对话框
+        this.dialogVisible = false;
+        //刷新菜单
+        this.getMenus();
+        //设置默认需要展开的菜单
+        this.expendedKey = [this.category.parentCid]
+      })
+    },
+    edit(data) {
+      console.log("要修改的数据", data);
+      this.dialogVisible = true;
+
+      this.dialogType = "edit";
+      this.title = "修改分类";
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: 'get',
+      }).then(({data}) => {
+        //请求成功后，从服务器获取的数据
+        console.log("从服务器返回的数据", data)
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+      })
+
+    },
     getMenus() {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
@@ -29,9 +118,16 @@ export default {
     },
     append(data) {
       console.log("append", data)
-      this.dialogVisible = true
+      this.title = "添加分类";
+      this.dialogType = "add";
+      this.dialogVisible = true;
       this.category.parentCid = data.catId;
+      this.category.name = "";
       this.category.cateLevel = data.cateLevel * 1 + 1;
+      this.category.sort = 0;
+      this.category.showStatus = 1;
+      this.category.icon = "";
+      this.category.productUnit = "";
     },
     //添加三级分类
     addCategory() {
@@ -46,7 +142,7 @@ export default {
           message: '保存菜单成功',
           type: 'success'
         });
-        this.dialogVisible=false;
+        this.dialogVisible = false;
         //刷新菜单
         this.getMenus();
         //设置默认需要展开的菜单
@@ -93,8 +189,18 @@ export default {
 
 <template>
   <div>
+    <el-switch
+      v-model="draggable"
+      active-text="开启拖拽"
+      inactive-text="关闭拖拽">
+    </el-switch>
+    <el-button @click="batchSave" v-if="draggable" type="primary">批量保存</el-button>
+    <el-button type="danger" @click="batchDelete">批量删除</el-button>
     <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId"
-             :default-expanded-keys="expendedKey">
+             :default-expanded-keys="expendedKey"
+             :draggable="draggable"
+             :allow-drop="allowDrop"
+             ref="menuTree"><!--ref唯一标识-->
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
@@ -103,31 +209,45 @@ export default {
             type="text"
             size="mini"
             @click="() => append(data)">
-            Append
+           增加
+          </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => edit(data)">
+            修改
           </el-button>
           <el-button v-if="node.childNodes.length==0"
                      type="text"
                      size="mini"
                      @click="() => remove(node, data)">
-            Delete
+            删除
           </el-button>
         </span>
       </span>
     </el-tree>
     <el-dialog
-      title="提示"
+      :title="title"
       :visible.sync="dialogVisible"
       width="30%"
+      :close-on-click-modal="false"
+
     >
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
 
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="addCategory">确 定</el-button>
+    <el-button type="primary" @click="submitData()">确 定</el-button>
   </span>
     </el-dialog>
   </div>
